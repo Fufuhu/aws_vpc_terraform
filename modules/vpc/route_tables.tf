b916587d3203d2ddcb8454a83b848b32d6167ff9
Ryoma Fujiwara <e238058r@hotmail.co.jp>
2022-04-19 16:19:48 +0900

    fix typo

--- modules/vpc/route_tables.tf
+++ modules/vpc/route_tables.tf
@@ -39,7 +39,7 @@ resource "aws_route_table" "private_route_tables" {
 }
 
 locals {
-  nat_gateway_id_in_case_of_redunduncy_disabled = aws_nat_gateway.nat_gateways[keys(aws_nat_gateway.nat_gateways)[0]].id
+  nat_gateway_id_in_case_of_redundancy_disabled = aws_nat_gateway.nat_gateways[keys(aws_nat_gateway.nat_gateways)[0]].id
 }
 
 # プライベートサブネット用のデフォルトルート設定
@@ -48,7 +48,7 @@ resource "aws_route" "private_default_route" {
   route_table_id = aws_route_table.private_route_tables[each.key].id
 
   destination_cidr_block = "0.0.0.0/0"
-  nat_gateway_id = var.nat_gateway_redundancy_enabled ? aws_nat_gateway.nat_gateways[each.value.availability_zone].id : local.nat_gateway_id_in_case_of_redunduncy_disabled
+  nat_gateway_id = var.nat_gateway_redundancy_enabled ? aws_nat_gateway.nat_gateways[each.value.availability_zone].id : local.nat_gateway_id_in_case_of_redundancy_disabled
 }
 
 # プライベートサブネットとプライベートサブネット用のルートテーブルのヒモ付
